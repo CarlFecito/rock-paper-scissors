1c842338407c8894c9146b70f4e9ed1e0fccb0e5
Carlos Molina <cmolinaf96@gmail.com>
2022-05-09 15:34:15 -0400

    added score count

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,18 +1,52 @@
-let playerScore = 0
+let playerScore = 0;
 let computerScore = 0;
+scoreDiv = document.getElementById('score')
 
+/* Selecting all the buttons and adding an event listener to each one. */
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id)
+    })
+})
+
+
+/* It returns a random "weapon" from the weapons array */
 function computerPlay() {
     const weapons = ['rock', 'paper', 'scissors'];
-    const computerPick = weapons[Math.floor(Math.random() * weapons.length)];
-    console.log(computerPick)
+    return weapons[Math.floor(Math.random() * weapons.length)];
 }
 
-function playRound(playerSelection)
+/* The function takes the player's selection and compares it to the computer's selection.
+ If the player wins, the player's score increases by one. 
+ If the computer wins, the computer's score increases by one. 
+ If the player and computer tie, nothing happens */
+function playRound(pSelect) {
     let computerSelection = computerPlay()
-    
-    if (playerSelection === "rock" &&  computerSelection === "paper" || 
-        playerSelection === "paper" &&  computerSelection === "scissors" ||
-        playerSelection === "scissors" &&  computerSelection === "rock" )
-        { 
-            console.log(loser)
+    console.log(computerSelection + "compSel")
+    console.log(pSelect + "pSel")
+    if (pSelect === computerSelection) {
+        updateScore()
+    } else if (
+        pSelect === "rock" &&  computerSelection === "paper" || 
+        pSelect === "paper" &&  computerSelection === "scissors" ||
+        pSelect === "scissors" &&  computerSelection === "rock" 
+        ) { 
+            computerScore++
+            updateScore()
+    } else if (
+        pSelect === "rock" &&  computerSelection === "scissors" || 
+        pSelect === "paper" &&  computerSelection === "rock" ||
+        pSelect === "scissors" &&  computerSelection === "paper"
+        ) {
+            playerScore++
+            updateScore()
+    }else {
+        alert("error")
     }
+}
+
+/* Updating the score on the page. */
+function updateScore() {
+    scoreDiv.innerHTML = playerScore + ':' + computerScore
+}
